1699b0a2be7620816abfb5f306f3eab936c93ddd
Marcelo Casse <marcelogabriel037@gmail.com>
2024-03-07 22:48:30 -0300

    update

--- unapausa/unapausa/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CheckList, HealthyHabit, Emotions, EmotionsLog, User
-
-
-admin.site.register([CheckList, HealthyHabit, Emotions, EmotionsLog, User])
